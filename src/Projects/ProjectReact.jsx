23fe85e554d28430171dbd3f83e30a968db46e33
SagarSingh01 <sagarsingh07789@gmail.com>
2026-04-30 23:09:25 +0530

    Changed Links

--- src/Projects/ProjectReact.jsx
+++ src/Projects/ProjectReact.jsx
@@ -9,7 +9,7 @@ const ProjectReact = [
         name: "Todo List (React)",
         description: "A responsive Todo List built with React for adding, deleting, and marking tasks complete. Includes local storage support.",
         tech: "React , Tailwind CSS",
-        liveUrl: "https://todo-app-react-1p2zqt1dv-sagarsingh01s-projects.vercel.app/",
+        liveUrl: "https://todo-app-react-khaki-eight.vercel.app/",
         repoUrl: "https://github.com/SagarSingh01/todo-app-react",
     },
     {
@@ -18,7 +18,7 @@ const ProjectReact = [
         image: ReactWeatherApp,
         description: "A modern weather app built with React and OpenWeather API to display real-time weather data and forecasts.",
         tech: "React , Tailwind CSS",
-        liveUrl: "https://weather-onwi0head-sagarsingh01s-projects.vercel.app/",
+        liveUrl: "https://weather-app-six-bice-56.vercel.app/",
         repoUrl: "https://github.com/SagarSingh01/weather-app",
     },
     {
